2fc066cf3361fdaf8fb1e8af39b8da7e8afdd652
Sherry Xie <sherryxie@Sherrys-MacBook-Pro.local>
2024-08-31 09:10:29 -0700

    Create project

---
+++ pokedex/pokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokedexApp.swift
+//  pokedex
+//
+//  Created by Sherry Xie on 8/31/24.
+//
+
+import SwiftUI
+
+@main
+struct pokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
